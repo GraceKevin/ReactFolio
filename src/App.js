ff25553bfa7a7262859715d43342af6a78828e3d
Kevin Grace <kgrace90@yahoo.com>
2021-11-17 20:44:03 -0500

    updated initial js for app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-import Header from './components/header';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Navigation/>
     </div>
   );
 }
