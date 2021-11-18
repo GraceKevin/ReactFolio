03e8c426670975bae92c8ebdab22c09e93471723
Kevin Grace <kgrace90@yahoo.com>
2021-11-17 21:08:48 -0500

    updated navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -2,21 +2,44 @@ import React from "react";
 
 const Navigation = () => {
     return (
-        <ul>
-            <li>
-                <a href="#about-me">About Me</a>
-            </li>
-            <li>
-                <a href="#work">Projects</a>
-            </li>
-            <li>
-                <a href="#contact">Contact Me</a>
-            </li>
-            <li>
-                <a href="#resume">Resume</a>
-            </li>
-        </ul>
+
+        <header>
+        <h1>
+
+            <a href="/" >KG</a>
+        </h1>
+
+        <nav>
+            
+            <ul>
+                <li>
+                    <a href="#about-me">About Me</a>
+                </li>
+                <li>
+                    <a href="#work">Projects</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact Me</a>
+                </li>
+                <li>
+                    <a href="#resume">Resume</a>
+                </li>
+            </ul>
+
+        </nav>
+
+    </header>
+
     )
 }
+
+<section id="about" class="intro">
+            <h2 class="section-title">About Me</h2>
+            <div class="flex-row">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                </p>
+            </div>
+        </section>
 
 export default Navigation 
