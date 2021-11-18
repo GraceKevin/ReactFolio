e4cb1a97631a5c6da091a860c3557f7d6ce3788e
Kevin Grace <kgrace90@yahoo.com>
2021-11-17 21:21:34 -0500

    added portfolio html section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Navigation from './components/Navigation';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Portfolio/>
     </div>
   );
 }
