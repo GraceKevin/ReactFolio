205fed3eed00627c4be4ea77e5786d06981b09a6
Kevin Grace <kgrace90@yahoo.com>
2021-11-17 21:26:30 -0500

    functioning contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+
+             <section id="contact" class="contact">
+            <h2 class="section-title">Contact</h2>
+            <div class="contact-flex">
+                <div class="contact-text">
+
+                    {/* <!--h3>Phone</h3>--> */}
+
+                    <a href="tel:4075066116"><p>407-506-6116</p></a>
+
+                </div>
+                <div class="contact-text">
+
+                    {/* <!--<h3>Email</h3>--> */}
+
+                    <a href="mailto:kgrace90@yahoo.com"><p>kgrace90@yahoo.com</p></a>
+                    
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default Contact
