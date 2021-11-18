8d575a9a00f33c24380db0602a28594761fa78c0
Kevin Grace <kgrace90@yahoo.com>
2021-11-17 21:24:54 -0500

    adjusted JS connection

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 
 import Navigation from './components/Navigation';
 import Portfolio from './components/Portfolio';
+import Footer  from './components/footer';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <Portfolio/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
