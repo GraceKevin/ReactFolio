65a666ec9a5fe017b6015f3042518082550bf732
Kevin Grace <kgrace90@yahoo.com>
2021-11-17 21:32:17 -0500

    updated Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,26 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
-      <h1>This is the footer</h1>
-    </div>
+    <footer>
+
+            <h2>© Kevin Grace</h2>
+
+            <div class="footer-flex">
+                {/* <!--  Linkedin Link --> */}
+                <a href="https://www.linkedin.com/in/kevin-grace-2a780267/" target="_blank">
+                    {/* <!-- https://brand.linkedin.com/downloads--> */}
+                    <img src="./assets/images/LI-Logo.png" alt="Linkedin logo."/>
+                </a>
+
+                {/* <!-- https://github.com/logos for Logo download --> */}
+                
+                <a href="https://github.com/GraceKevin" target="_blank">
+                    {/* <!-- https://www.freepnglogos.com/images/512x512-logo-27148.html --> */}
+                    <img src="./assets/images/GitHub-Mark-Light-120px-plus.png" class="github" alt="Github." />
+                </a>
+                
+            </div>
+        </footer>
   );
 }
 
